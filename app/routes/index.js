442777c0e2934ec495009e554f711f3dc6bb54f9
Naman Jain <naman-jain@outlook.com>
2020-03-22 00:13:32 +0100

    added pre-commit hooks

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,9 @@
-let express = require('express');
+let express = require("express");
 let router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) 
-{
-    res.render('index', { title: 'Express' });
+router.get("/", function(req, res) {
+    res.render("index", { title: "Express" });
 });
 
 module.exports = router;
