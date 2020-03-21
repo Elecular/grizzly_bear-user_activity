60f9c475a7991f2799910ae7935172b226f37cf1
Naman Jain <naman-jain@outlook.com>
2020-03-21 11:44:53 +0100

    Commit

---
+++ app/bin/deploy-stage.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+curl --location --request POST 'https://api.github.com/repos/nj20/grizzly_bear-user_activity/deployments' \
+--header "Authorization: token $1" \
+--data-raw '{
+  "ref": "master",
+  "payload": {
+  	"gke_cluster": "grizzly-bear-stage",
+  	"gke_zone": "us-central1-c"
+  },
+  "environment": "stage",
+  "description": "Deploy master on staging"
+}'
