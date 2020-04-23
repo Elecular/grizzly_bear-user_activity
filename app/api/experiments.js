b03f8ae795ec1b10d34bb935f744e4dea9d74160
Naman Jain <naman-jain@outlook.com>
2020-04-23 10:22:00 +0200

    Updated experiments api and acceptance test

---
+++ app/api/experiments.js
@@ -0,0 +1,13 @@
+/*const fetch = require('node-fetch');
+//const host = process.env["PRIVATE_EXPERIMENTS_SERVICE_HOST"];
+//const port = process.env["PRIVATE_EXPERIMENTS_SERVICE_PORT"];
+
+/**
+ * Validates if the given ownerid is the actual owner of the given project
+ * @param {String} ownerId
+ * @param {String} projectId
+ * 
+ */
+//module.exports.validateOwner = async (ownerId, projectId) => {
+//fetch(`${host}:${port}/`)
+//}
